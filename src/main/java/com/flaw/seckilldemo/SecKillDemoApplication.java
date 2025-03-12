1a3260b36794ca63cdfef8827ff52bb943f7bb99
yulei <yulei@163.com>
2025-03-12 14:43:53 +0800

    初始化项目

--- src/main/java/com/flaw/seckilldemo/SecKillDemoApplication.java
+++ src/main/java/com/flaw/seckilldemo/SecKillDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeckillDemoApplication {
+public class SecKillDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeckillDemoApplication.class, args);
+        SpringApplication.run(SecKillDemoApplication.class, args);
     }
 
 }
